40ef4d1444eb65e8bf75779c0b65c0e1d3fcb9f7
drstarson <dimlyus@gmail.com>
2024-09-01 22:46:28 -0700

    Initial commit

---
+++ Demo_Onboarding_Quiz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Demo_Onboarding_Quiz
+//
+//  Created by Dmitriy Richard Starson on 9/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Demo_Onboarding_Quiz/Demo_Onboarding_QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_Onboarding_QuizApp.swift
+//  Demo_Onboarding_Quiz
+//
+//  Created by Dmitriy Richard Starson on 9/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Demo_Onboarding_QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
