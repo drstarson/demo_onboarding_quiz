0feebee2be8391725be650c6a301aeae770b2a8b
drstarson <dmitriy@passiolife.com>
2024-09-01 23:18:08 -0700

    testing

--- Demo_Onboarding_Quiz/ContentView.swift
+++ Demo_Onboarding_Quiz/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello World Test!")
+            Text("Hello World Test 2!")
         }
         .padding()
     }
